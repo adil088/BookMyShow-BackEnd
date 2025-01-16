7fd6ccb625c24c5d38c833971d612b88c6475873
aadil <adil88.mohd@gmail.com>
2025-01-16 13:11:28 +0530

    Mail: Initial Code

---
+++ src/test/java/com/bookmyshow_mail/Book_my_show_mail/BookMyShowMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_mail.Book_my_show_mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
