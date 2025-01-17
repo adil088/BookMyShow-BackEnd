509f0ca68ae51b87541677f2ffaba6bf1f1ea74d
aadil <adil88.mohd@gmail.com>
2025-01-18 03:01:19 +0530

    DBAPI: Created Theater model

---
+++ src/main/java/com/bookmyshow_db/Book_my_show_db/models/Theater.java
@@ -0,0 +1,36 @@
+package com.bookmyshow_db.Book_my_show_db.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Theater {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID TheaterId;
+    @ManyToOne
+    AppUser owner;
+    String address;
+    @Column(unique = true)
+    Long theaterHelpline;
+
+}
